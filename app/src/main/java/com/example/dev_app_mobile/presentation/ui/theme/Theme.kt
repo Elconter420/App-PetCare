65e1f1cb3fb02490b423a8f6d496374cfe21b370
Maryu12 <maryury.villa@correounivalle.edu.co>
2025-11-28 12:35:56 -0500

    Add PlansScreen, PlanCard implementation and change for Theme

--- app/src/main/java/com/example/dev_app_mobile/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dev_app_mobile/presentation/ui/theme/Theme.kt
@@ -10,32 +10,25 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+// Tema oscuro usando la paleta
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PetCareDark,
+    secondary = PetCareSecondary,
+    tertiary = PetCarePrimary,
 )
 
+//Tema claro usando la paleta
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PetCarePrimary,
+    secondary = PetCareSecondary,
+    tertiary = PetCareDark,
+    // si quieres luego puedes ajustar background/surface/onPrimary, etc.
 )
 
 @Composable
 fun DevappmobileTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    // Colores dinámicos (Android 12+)
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -44,7 +37,6 @@ fun DevappmobileTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
